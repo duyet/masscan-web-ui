c127b8692ef445eb3e1983e78355036df3d04271
Van-Duyet Le <lvduit08@gmail.com>
2015-12-06 01:53:05 +0700

    Update config for heroku

--- includes/config.php
+++ includes/config.php
@@ -12,12 +12,23 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 /**
  * Database related configuration
- */
+*/
+$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
 define('DB_DRIVER',	'MySQL');
-define('DB_HOST',	'localhost');
-define('DB_USERNAME',	'masscan');
-define('DB_PASSWORD', 	'changem3');
-define('DB_DATABASE', 	'masscan');
+
+if ($db) {
+	define('DB_HOST',	$db['host']);
+	define('DB_USERNAME',	 $db['user']);
+	define('DB_PASSWORD', 	$db['pass']);
+	define('DB_DATABASE', 	substr($db["path"], 1));
+} else {
+	define('DB_HOST',	'localhost');
+	define('DB_USERNAME',	'masscan');
+	define('DB_PASSWORD', 	'changem3');
+	define('DB_DATABASE', 	'masscan');
+}
+
 /**
  * Include the db class
  */
